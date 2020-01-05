075e76c65a44ea5db69e1b3a697f60ba7f61e273
Pradeep <pradeepbt.bmk@gmail.com>
2020-01-05 13:13:34 +0530

    Creation

---
+++ movie-info-service/src/main/java/com/micropradeep/movieinfoservice/MovieResources.java
@@ -0,0 +1,27 @@
+package com.micropradeep.movieinfoservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.micropradeep.movieinfoservice.model.Movie;
+import com.micropradeep.movieinfoservice.model.MovieSummary;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieResources {
+	@Value("${api.key}")
+    private String apiKey;
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@RequestMapping("/{movieId}")
+	public  Movie getMovieInfo(@PathVariable("movieId") int movieId)
+	{
+		  MovieSummary movieSummary = restTemplate.getForObject("https://api.themoviedb.org/3/movie/" + movieId + "?api_key=" +  apiKey, MovieSummary.class);
+	        return new Movie(movieId, movieSummary.getTitle(), movieSummary.getOverview());
+	}
+}
